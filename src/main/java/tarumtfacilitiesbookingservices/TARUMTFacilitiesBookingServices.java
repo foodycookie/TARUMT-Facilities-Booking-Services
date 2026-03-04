bf79d76a81687660d86e9c32bb540a09fce298be
foodycookie <onghao.howard@gmail.com>
2026-03-04 20:08:13 +0800

    Hello World

---
+++ src/main/java/tarumtfacilitiesbookingservices/TARUMTFacilitiesBookingServices.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package tarumtfacilitiesbookingservices;
+
+/**
+ *
+ * @author Ong Hao Howard
+ */
+public class TARUMTFacilitiesBookingServices {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

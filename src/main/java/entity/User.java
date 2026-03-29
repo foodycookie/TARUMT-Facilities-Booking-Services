bce3db6a03747ef178561d2179ae95dbb72b2367
hui-12 <laiyh091204@gmail.com>
2026-03-29 20:29:20 +0800

    add user class

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,68 @@
+package entity;
+
+import java.io.Serializable;
+
+public class User implements Serializable, Comparable<User>{
+    private int userID;
+    private String userName;
+    
+    public User() {}
+    
+    public User (int userID, String username) {
+        this.userID = userID;
+        this.userName = userName;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "userID=" + userID + ", userName=" + userName + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final User other = (User) obj;
+        return this.userID == other.userID;
+    }
+    
+    @Override
+    public int compareTo(User o) {
+        int result = Integer.compare(this.userID, o.userID);
+        if (result != 0) {
+            return result;
+        }
+        
+        return this.userName.compareToIgnoreCase(o.userName);
+    }
+    
+}

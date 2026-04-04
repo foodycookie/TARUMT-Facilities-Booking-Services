f7253f3a20f626d61ab9ccd7eb4479ff62674bbd
foodycookie <onghao.howard@gmail.com>
2026-04-04 20:47:14 +0800

    - Changes in Timeslot related file - Minor bug fixes

--- src/main/java/utility/SortedListHelper.java
+++ src/main/java/utility/SortedListHelper.java
@@ -1,13 +1,67 @@
 package utility;
 
+import adt.SortedArrayList;
+import entity.Facility;
+import entity.Timeslot;
+import java.time.LocalDate;
+
 /**
  * @author Ong Hao Howard
  */
 
 public class SortedListHelper {
-    // Let the sorted list be: 1, 1, 1, 2, 2, 2, 3, 3, 3
-    // If my target is 2, since this is a sorted list, there will be no reason to check pass 2
-    public static boolean compareStringIfExceedTarget(String firstData, String secondData) {
-        return firstData.compareToIgnoreCase(secondData) > 0;
+   public static <T extends Comparable<T>> int findStartPosition(SortedArrayList<T> list, T probe) {
+       int position = list.getPosition(probe);
+
+       if (position >= 1) {
+           // Found
+           return position;
+       } 
+       
+       else {
+           // Not found, return insertion point
+           return -(position);
+       }
+   }
+   
+    // Checks whether iteration should stop early when scanning a timeslot list
+    public static boolean isPastTargetDate(Timeslot timeslot, LocalDate date) {
+        return timeslot.getDate().isAfter(date);
+    }
+    
+    public static boolean isPastTargetDateAndFacilityName(Timeslot timeslot, Facility facility, LocalDate date) {
+        if (timeslot.getDate().isAfter(date)) return true;
+        if (timeslot.getDate().isBefore(date)) return false;
+
+        int facilityNameComparison = timeslot.getFacility().getFacilityName().compareToIgnoreCase(facility.getFacilityName());        
+        return facilityNameComparison > 0;
+    }
+    
+    public static boolean isPastTargetDateAndRoomType(Timeslot timeslot, Facility facility, LocalDate date) {
+        if (timeslot.getDate().isAfter(date)) return true;
+        if (timeslot.getDate().isBefore(date)) return false;
+
+        int facilityNameComparison = timeslot.getFacility().getFacilityName().compareToIgnoreCase(facility.getFacilityName());
+        if (facilityNameComparison > 0) return true;
+        if (facilityNameComparison < 0) return false;
+
+        int roomTypeComparison = timeslot.getFacility().getRoomType().compareToIgnoreCase(facility.getRoomType());
+        return roomTypeComparison > 0;        
+    }
+    
+    public static boolean isPastTargetDateAndRoomName(Timeslot timeslot, Facility facility, LocalDate date) {
+        if (timeslot.getDate().isAfter(date)) return true;
+        if (timeslot.getDate().isBefore(date)) return false;
+
+        int facilityNameComparison = timeslot.getFacility().getFacilityName().compareToIgnoreCase(facility.getFacilityName());
+        if (facilityNameComparison > 0) return true;
+        if (facilityNameComparison < 0) return false;
+
+        int roomTypeComparison = timeslot.getFacility().getRoomType().compareToIgnoreCase(facility.getRoomType());
+        if (roomTypeComparison > 0) return true;
+        if (roomTypeComparison < 0) return false;
+
+        int roomNameComparison = timeslot.getFacility().getRoomName().compareToIgnoreCase(facility.getRoomName());
+        return roomNameComparison > 0;
     }
 }

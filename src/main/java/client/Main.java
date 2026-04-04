ab828082fb8c5e10fcfc84fdab65326a5e55aa7a
hui-12 <laiyh091204@gmail.com>
2026-04-04 16:50:14 +0800

    create main menu(link all module)

--- src/main/java/client/Main.java
+++ src/main/java/client/Main.java
@@ -1,18 +1,50 @@
 package client;
 
 import boundary.UserMaintenanceUI;
+import boundary.FacilityMaintenanceUI;
 import control.BookingMaintenance;
+import utility.Validation;
 
 /**
  *
  * @author TAY TIAN YOU
  */
 public class Main {
-    public static void main(String[] args){
-        UserMaintenanceUI ui = new UserMaintenanceUI();
-        ui.start();
-        
-//        BookingMaintenance booking = new BookingMaintenance();
-//        booking.runBooking();
+    public void main(String[] args){
+         int choice;
+
+        do {
+            System.out.println("\n========== MAIN MENU ==========");
+            System.out.println("1. USER MANAGEMENT");
+            System.out.println("2. FACILITY MANAGEMENT");
+            System.out.println("3. BOOKING");
+            System.out.println("4. TIME SLOT");
+            System.out.println("0. Exit");
+            System.out.print("Enter choice: ");
+            choice = Validation.readInt();
+           
+            switch (choice) {
+                case 1 :
+                    UserMaintenanceUI UserUi = new UserMaintenanceUI();
+                    UserUi.start();
+                    
+                case 2 :
+                    FacilityMaintenanceUI FacilityUi = new FacilityMaintenanceUI();
+                    FacilityUi.start();
+                    
+                case 3 :
+                    BookingMaintenance booking = new BookingMaintenance();
+                    booking.runBooking();
+                    
+                case 4 :
+                    System.out.println("No link yet...");
+                    
+                case 0 :
+                    System.out.println("Exiting User Management Module...");
+                    
+                default :
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        } while (choice != 0);
     }
 }
